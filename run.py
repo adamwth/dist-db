4fe8e98286571a14f63667a3b1706c05ebcc9ba1
Adam Chew <adamwth@hotmail.com>
2020-10-09 17:34:43 +0800

    feat: add db arg

--- run.py
+++ run.py
@@ -13,13 +13,14 @@ def readAndExecSql(conn, file):
     with conn.cursor() as cur:
         try:
             cur.execute(file.read())
+            print(cur.fetchall())
         except psycopg2.Error as e:
             logging.debug(e)
     conn.commit()
 
 
 def main():
-    # Usage: python3 setup.py <file> <hostNum>
+    # Usage: python3 setup.py <file> <hostNum> <db>
     # Example: python3 setup.py create-tables.sql 2
     # if hostNum not specified, use default host 2 (i.e. xcnc2)
     parser = argparse.ArgumentParser()
@@ -30,12 +31,16 @@ def main():
                         type=int, default=2,
                         help='Host number e.g. 2 for xcnc2. Default is xcnc2.'
                         )
+    parser.add_argument("-db", '--database',
+                        type=str, default="project",
+                        help='Database name'
+                        )
     args = parser.parse_args()
 
     host = f'xcnc{args.hostNum}.comp.nus.edu.sg'
     port = '26259'
     user = 'admin'  # use admin so we don't have to grant privileges manually
-    database = 'project'
+    database = args.database
     conn = psycopg2.connect(host=host,
                             port=port,
                             user=user,
