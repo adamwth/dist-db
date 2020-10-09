92ef02cdfcf54691f3a302f6a13c9fb442dc4fd1
Adam Chew <adamwth@hotmail.com>
2020-10-09 12:07:04 +0800

    feat: use argparse

--- run.py
+++ run.py
@@ -4,40 +4,44 @@ import time
 import logging
 import random
 import sys
+import argparse
 
 logging.basicConfig(level=logging.DEBUG)
 
 
-def readAndExecSql(conn, filename):
-    with open(filename, 'r') as fd:
-        sqlFile = fd.read()
-
-    sqlCommands = sqlFile.split(';')
-
+def readAndExecSql(conn, file):
     with conn.cursor() as cur:
-        for command in sqlCommands:
-            # cur.execute(command)
-            try:
-                cur.execute(command)
-            except psycopg2.Error as e:
-                logging.debug(e)
+        try:
+            cur.execute(file.read())
+        except psycopg2.Error as e:
+            logging.debug(e)
     conn.commit()
 
 
 def main():
-    # Usage: python3 setup.py <hostNum>
+    # Usage: python3 setup.py <hostNum> <file>
     # Example: python3 setup.py 2
     # if arg not specified, use default host 2
-    hostNum = 2 if len(sys.argv) <= 1 else sys.argv[1]
-    host = f'xcnc{hostNum}.comp.nus.edu.sg'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("hostNum", metavar='H',
+                        type=int,
+                        help='Host number e.g. 2 for xcnc2'
+                        )
+    parser.add_argument("file", metavar="F",
+                        type=argparse.FileType('r'),
+                        help='File with SQL to be executed')
+    args = parser.parse_args()
+
+    host = f'xcnc{args.hostNum}.comp.nus.edu.sg'
     port = '26259'
     user = 'admin'  # use admin so we don't have to grant privileges manually
-    database = 'bank'
+    database = 'project'
     conn = psycopg2.connect(host=host,
                             port=port,
                             user=user,
                             database=database)
-    readAndExecSql(conn, 'test.sql')
+    readAndExecSql(conn, args.file)
+    args.file.close()
 
 
 if __name__ == '__main__':
