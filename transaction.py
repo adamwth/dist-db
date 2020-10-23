4645db86d42d5ddc29ccb05eaf1539655b622152
John Phua <phuajohn0@gmail.com>
2020-10-24 01:12:30 +0800

    Add WIP implementation for new order transaction

--- transaction.py
+++ transaction.py
@@ -3,6 +3,14 @@ from datetime import datetime
 from abc import ABC, abstractmethod
 
 
+# Returns "(%s, %s, ...), (%s, %s, ...)" meant to be used for VALUES
+# args determines number of args in each row
+# rows determines number of rows for VALUES
+def create_values_placeholder(args, rows):
+    single_row = "(" + ("%s," * args)[:-1] + "),"
+    return (single_row * rows)[:-1]
+
+
 class Transaction(ABC):
     def __init__(self, metrics_manager):
         self.inputs = {}
@@ -35,14 +43,135 @@ class NewOrderTransaction(Transaction):
     def __init__(self, metrics_manager, conn, inputs):
         super().__init__(metrics_manager)
         self.conn = conn
-        # TODO: handle input parsing here
+        self.warehouse_id = int(inputs[0])
+        self.district_id = int(inputs[1])
+        self.customer_id = int(inputs[2])
+
+        num_items = int(inputs[3])
+        self.items = {}
+        for i in range(num_items):
+            start_index = i * 3 + 2
+            new_item = {
+                "item_no": int(inputs[start_index]),
+                "supplying_warehouse_no": inputs[start_index + 1],
+                "quantity": inputs[start_index + 2],
+                "ol_number": i
+            }
+            self.items[new_item["item_no"]] = new_item
 
     def run(self):
         with self.conn:
             with self.conn.cursor() as curs:
                 self.start()
-                # TODO: handle txn here
-                self.end()
+                # Get district information, next order id
+                curs.execute("SELECT D_NEXT_O_ID, D_TAX FROM District WHERE D_W_ID=%s AND D_ID=%s;",
+                             (self.warehouse_id, self.district_id))
+                order_id, district_tax = curs.fetchone()
+
+                # Update district information
+                curs.execute("UPDATE District SET D_NEXT_O_ID=%s WHERE D_W_ID=%s AND D_ID=%s;",
+                             (order_id + 1, self.warehouse_id, self.district_id))
+
+                # Create new order entry
+                all_local = 1 if all(
+                    [x["supplying_warehouse_no"] == self.warehouse_id for x in self.items.values()]) else 0
+                entry_date = datetime.now()
+                curs.execute("INSERT INTO Order VALUES (%s, %s, %s, %s, %s, %s, %s, %s);", (
+                    self.warehouse_id, self.district_id, order_id, self.customer_id, None, len(self.items), all_local,
+                    entry_date))
+
+                # Retrieve stock information for items
+                total_amount = 0
+                values_placeholder = create_values_placeholder(2, len(self.items))
+                stock_keys = []
+                for item in self.items.values():
+                    stock_keys.append(self.warehouse_id)
+                    stock_keys.append(item["item_no"])
+                curs.execute(
+                    "SELECT S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT, S_DIST_%s FROM Stock WHERE (S_W_ID, S_I_ID) IN (VALUES " + values_placeholder + ");",
+                    (str(self.district_id).zfill(2), *stock_keys))
+                stocks = curs.fetchall()
+
+                updated_stocks = []
+                for stock in stocks:
+                    wh_id, i_id, qty, ytd, order_cnt, remote_cnt, dist_info = stock
+                    order_qty = self.items[i_id]["quantity"]
+                    supply_warehouse_id = self.items[i_id]["supplying_warehouse_no"]
+
+                    new_qty = qty - order_qty
+                    if new_qty < 10:
+                        new_qty += 100
+                    new_remote_cnt = remote_cnt
+                    if supply_warehouse_id != self.warehouse_id:
+                        new_remote_cnt += 1
+
+                    updated_stocks.append(
+                        (wh_id, i_id, new_qty, ytd + order_qty, order_cnt + 1, new_remote_cnt, dist_info))
+                    # Keep relevant info for item
+                    self.items[i_id]["stocks"] = {
+                        "quantity": new_qty,
+                        "dist_info": dist_info,
+                    }
+
+                # Update stock information for items (use upsert for single update)
+                values_placeholder = create_values_placeholder(6, len(updated_stocks))
+                updated_stocks_vals = []
+                for stock in updated_stocks:
+                    updated_stocks_vals.extend(stock[:-1])
+                curs.execute(
+                    "UPSERT INTO Stock (S_W_ID, S_I_ID, S_QUANTITY, S_YTD, S_ORDER_CNT, S_REMOTE_CNT) VALUES " + values_placeholder + ";",
+                    updated_stocks_vals)
+
+                # Retrieve item information, track item costs for order
+                values_placeholder = create_values_placeholder(len(self.items), 1)
+                curs.execute("SELECT I_ID, I_NAME, I_PRICE FROM Item WHERE I_ID IN " + values_placeholder + ";",
+                             list(self.items.keys()))
+                items = curs.fetchall()
+                for item in items:
+                    i_id, name, price = item
+                    self.items[i_id]["cost"] = price * self.items[i_id]["quantity"]
+                    self.items[i_id]["name"] = name
+                    total_amount += self.items[i_id]["cost"]
+
+                # Add new order lines
+                values_placeholder = create_values_placeholder(10, len(self.items))
+                new_order_lines = []
+                for item in self.items.values():
+                    new_order_lines.extend([self.warehouse_id, self.district_id, order_id, item["ol_number"],
+                                            item["item_no"], None, item["cost"], item["supplying_warehouse_no"],
+                                            item["quantity"], item["stocks"]["dist_info"]])
+                curs.execute("INSERT INTO OrderLine VALUES " + values_placeholder + ";")
+
+                # Retrieve user information
+                curs.execute(
+                    "SELECT C_LAST, C_CREDIT, C_DISCOUNT FROM Customer WHERE C_W_ID=%s AND C_D_ID=%s AND C_ID=%s",
+                    (self.warehouse_id, self.district_id, self.customer_id))
+                last_name, credit, discount = curs.fetchone()
+
+                # Retrieve warehouse tax
+                curs.execute("SELECT W_TAX FROM Warehouse WHERE W_ID=%s ", (self.warehouse_id,))
+                warehouse_tax = curs.fetchone()
+
+                # Calculate total amount
+                total_amount = total_amount * (1 + district_tax + warehouse_tax) * (1 - discount)
+
+                # Add to output dict
+                self.outputs["Customer identifier"] = "(%d, %d, %d)".format(self.warehouse_id, self.district_id,
+                                                                            self.customer_id)
+                self.outputs["Customer last name"] = last_name
+                self.outputs["Customer credit"] = credit
+                self.outputs["Customer discount"] = discount
+                self.outputs["Warehouse tax rate"] = warehouse_tax
+                self.outputs["District tax rate"] = district_tax
+                self.outputs["Order number"] = order_id
+                self.outputs["Entry date"] = entry_date
+                self.outputs["Num items"] = len(self.items)
+                for item in self.items.values():
+                    item_name = "Item %s".format(item["item_no"])
+                    self.outputs[item_name] = "%s x %d from warehouse %d costing %d with remaining stock %d".format(
+                        item["name"], item["quantity"], item["supplying_warehouse_no"], item["cost"],
+                        item["stocks"]["quantity"])
+        self.end()
 
 
 class PaymentTransaction(Transaction):
