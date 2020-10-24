f00892595b561362ad2fa2c5f7851aa583a5ce1c
John Phua <phuajohn0@gmail.com>
2020-10-24 15:01:40 +0800

    Add WIP implementation of Payment Transaction

--- transaction.py
+++ transaction.py
@@ -144,12 +144,12 @@ class NewOrderTransaction(Transaction):
 
                 # Retrieve user information
                 curs.execute(
-                    "SELECT C_LAST, C_CREDIT, C_DISCOUNT FROM Customer WHERE C_W_ID=%s AND C_D_ID=%s AND C_ID=%s",
+                    "SELECT C_LAST, C_CREDIT, C_DISCOUNT FROM Customer WHERE C_W_ID=%s AND C_D_ID=%s AND C_ID=%s;",
                     (self.warehouse_id, self.district_id, self.customer_id))
                 last_name, credit, discount = curs.fetchone()
 
                 # Retrieve warehouse tax
-                curs.execute("SELECT W_TAX FROM Warehouse WHERE W_ID=%s ", (self.warehouse_id,))
+                curs.execute("SELECT W_TAX FROM Warehouse WHERE W_ID=%s;", (self.warehouse_id,))
                 warehouse_tax = curs.fetchone()
 
                 # Calculate total amount
@@ -178,14 +178,60 @@ class PaymentTransaction(Transaction):
     def __init__(self, metrics_manager, conn, inputs):
         super().__init__(metrics_manager)
         self.conn = conn
-        # TODO: handle input parsing here
+        self.warehouse_id = int(inputs[0])
+        self.district_id = int(inputs[1])
+        self.customer_id = int(inputs[2])
+        self.payment = int(inputs[3])
 
     def run(self):
         with self.conn:
             with self.conn.cursor() as curs:
                 self.start()
-                # TODO: handle txn here
-                self.end()
+                # Fetch and update warehouse details
+                curs.execute(
+                    "SELECT W_YTD, W_STREET_1, W_STREET_2, W_CITY, W_STATE, W_ZIP FROM Warehouse WHERE W_ID=%s;",
+                    (self.warehouse_id,))
+                w_ytd, w_street1, w_street2, w_city, w_state, w_zip = curs.fetchone()
+                curs.execute("UPDATE Warehouse SET W_YTD = %s WHERE W_ID=%s;",
+                             (w_ytd + self.payment, self.warehouse_id))
+
+                # Fetch and update district details
+                curs.execute(
+                    "SELECT D_YTD, D_STREET_1, D_STREET_2, D_CITY, D_STATE, D_ZIP FROM District WHERE D_W_ID=%s AND D_ID=%s;",
+                    (self.warehouse_id, self.district_id))
+                d_ytd, d_street1, d_street2, d_city, d_state, d_zip = curs.fetchone()
+                curs.execute("UPDATE District SET D_YTD = %s WHERE D_W_ID=%s AND D_ID=%s;",
+                             (d_ytd + self.payment, self.warehouse_id, self.district_id))
+
+                # Fetch and update customer details
+                curs.execute(
+                    "SELECT C_FIRST, C_MIDDLE, C_LAST, C_STREET_1, C_STREET_2, C_CITY, C_STATE, C_ZIP, C_PHONE, C_SINCE, C_CREDIT, C_CREDIT_LIM, C_DISCOUNT, C_BALANCE, C_YTD_PAYMENT, C_PAYMENT_CNT FROM Customer WHERE C_W_ID=%s AND C_D_ID=%s AND C_ID=%s;",
+                    (self.warehouse_id, self.district_id, self.customer_id))
+                first_name, middle_name, last_name, c_street1, c_street2, c_city, c_state, c_zip, c_phone, c_since, c_credit, c_credit_lim, c_discount, c_balance, c_ytd, c_payment_count = curs.fetchone()
+                new_balance = c_balance - self.payment
+                new_ytd_payment = c_ytd + self.payment
+                new_payment_cnt = c_payment_count + 1
+                curs.execute(
+                    "UPDATE Customer SET C_BALANCE=%s, C_YTD_PAYMENT=%s, C_PAYMENT_CNT=%s WHERE C_W_ID=%s AND C_D_ID=%s AND C_ID=%s;",
+                    (new_balance, new_ytd_payment, new_payment_cnt, self.warehouse_id, self.district_id,
+                     self.customer_id))
+
+                # Add to output dict
+                self.outputs["Customer identifier"] = "(%d, %d, %d)".format(self.warehouse_id, self.district_id,
+                                                                            self.customer_id)
+                self.outputs["Customer name"] = "%s %s %s".format(first_name, middle_name, last_name)
+                self.outputs["Customer address"] = "%s %s %s %s %s".format(c_street1, c_street2, c_city, c_state, c_zip)
+                self.outputs["Customer phone"] = c_phone
+                self.outputs["Customer creation date"] = c_since
+                self.outputs["Customer credit"] = c_credit
+                self.outputs["Customer credit limit"] = c_credit_lim
+                self.outputs["Customer discount"] = c_discount
+                self.outputs["Customer balance"] = new_balance
+                self.outputs["Warehouse address"] = "%s %s %s %s %s".format(w_street1, w_street2, w_city, w_state,
+                                                                            w_zip)
+                self.outputs["District address"] = "%s %s %s %s %s".format(d_street1, d_street2, d_city, d_state, d_zip)
+                self.outputs["Payment"] = self.payment
+        self.end()
 
 
 class DeliveryTransaction(Transaction):
