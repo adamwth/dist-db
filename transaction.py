ab38f89f583eee0b216553ac739fa8b6f32ccd0d
John Phua <phuajohn0@gmail.com>
2020-10-25 12:40:46 +0800

    fix: order-status transaction

--- transaction.py
+++ transaction.py
@@ -346,27 +346,29 @@ class OrderStatusTransaction(Transaction):
                 first_name, middle_name, last_name, balance = curs.fetchone()
 
                 # Fetch items in last order
-                curs.execute(
-                    "SELECT o_id, o_entry_d, o_carrier_id, ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_delivery_d FROM order JOIN orderline WHERE ol_w_id = o_w_id AND ol_d_id = o_d_id AND ol_o_id = o_id AND o_c_id = c_id ORDER BY o_entry_d DESC LIMIT 1;")
+                curs.execute("""
+                SELECT o_id, o_entry_d, o_carrier_id, ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_delivery_d 
+                FROM "order" 
+                JOIN orderline ON ol_w_id = o_w_id AND ol_d_id = o_d_id AND ol_o_id = o_id  
+                WHERE o_w_id=%s AND o_d_id=%s AND o_c_id=%s ORDER BY o_id DESC LIMIT 1;
+                """, (self.warehouse_id, self.district_id, self.customer_id))
                 items = curs.fetchall()
 
                 # Add to output
-                self.outputs["Customer name"] = "%s %s %s".format(
-                    first_name, middle_name, last_name)
+                self.outputs["Customer name"] = "{} {} {}".format(first_name, middle_name, last_name)
                 self.outputs["Customer balance"] = balance
 
+                if len(items) > 0:
+                    order_name = "Order {}".format(items[0][0])
+                    self.outputs[order_name] = "ordered at {} with carrier {}".format(
+                        items[0][1], items[0][2])
                 order_items = []
                 for item in items:
                     order_item = {"number": item[3], "supplying_warehouse": item[4], "quantity": item[5],
                                   "price": item[6], "delivery_date": item[7]}
                     order_items.append(order_item)
-                    item_name = "Item %s".format(item[3])
-                    self.outputs[item_name] = "%d from warehouse %d costing %d delivered at %d".format(item[5], item[4],
-                                                                                                       item[6], item[7])
-                if len(items) > 0:
-                    order_name = "Order %s".format(item[0])
-                    self.outputs[order_name] = "ordered at %s with carrier %d".format(
-                        item[1], item[2])
+                self.outputs["Order items"] = order_items
+
         self.end()
 
 
