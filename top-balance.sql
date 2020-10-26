04973bd6e40e179cb7c3e58c8000b5ee80799981
John Phua <phuajohn0@gmail.com>
2020-10-26 22:46:24 +0800

    opt: top-balance transaction

--- top-balance.sql
+++ top-balance.sql
@@ -1,3 +1,20 @@
+WITH top_customers AS (
+SELECT
+    c_w_id,
+    c_d_id,
+    c_id,
+    c_first,
+    c_middle,
+    c_last,
+    c_balance
+FROM
+    customer
+ORDER BY
+    c_balance DESC
+LIMIT
+    10
+)
+
 SELECT
     c.c_first,
     c.c_middle,
@@ -6,11 +23,7 @@ SELECT
     w.w_name,
     d.d_name
 FROM
-    customer c
+    top_customers c
     JOIN district d ON c.c_w_id = d.d_w_id
     AND c.c_d_id = d.d_id
-    JOIN warehouse w ON d.d_w_id = w.w_id
-ORDER BY
-    c.c_balance DESC
-LIMIT
-    10;
+    JOIN warehouse w ON d.d_w_id = w.w_id;
