42e5333aa1f278f830632e1c8212cc5321f0fa34
John Phua <phuajohn0@gmail.com>
2020-10-30 22:14:31 +0800

    fix: Run clients on different servers

--- run-experiment.sh
+++ run-experiment.sh
@@ -4,6 +4,6 @@ num_clients=$1
 
 for ((i=1; i<=$num_clients; i++));
 do
-	echo "Running on ${i}.txt"
-	nohup python3 client.py xact-files/${i}.txt >${i}_output.out 2>${i}_stats.out &
+	echo "Running on ${i}.txt, host num $(((${i} % 5) + 1))"
+	nohup python3 client.py xact-files/${i}.txt -hn $(((${i} % 5) + 1)) >${i}_output.out 2>${i}_stats.out &
 done
